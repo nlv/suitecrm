9002f2572d26334252e78b94fe151260e188a0ab
Leon Nikitin <nlv@lab321.ru>
2016-07-16 00:48:43 +0600

    Изменения после восстановления

--- config_override.php
+++ config_override.php
@@ -3,4 +3,6 @@
 $sugar_config['default_date_format'] = 'd.m.Y';
 $sugar_config['disable_persistent_connections'] = false;
 $sugar_config['default_time_format'] = 'H:i';
+$sugar_config['js_custom_version'] = 2;
+$sugar_config['js_lang_version'] = 2;
 /***CONFIGURATOR***/
